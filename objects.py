3b8768be159acac8f11196ec62c9c7b75d49efc0
James ODonnell <mr.odonnell.cte@gmail.com>
2021-05-10 13:19:58 -0400

    Add files via upload

---
+++ objects.py
@@ -0,0 +1,45 @@
+class Fred():
+	"""fred class variables"""
+
+  def reset(self, x):
+    """reset"""
+
+  def moveLeft(self, leftBound):
+	"""moveLeft"""
+
+  def moveRight(self, rightBound):
+	"""moveRight"""
+    
+
+  def loadImages(self, pygame):
+	"""loadImages"""
+
+  def draw(self, surface, time):
+	""" draw """
+	
+  def __init__(self, x):
+    """init"""
+
+class Barrel():
+
+  """variables"""
+
+  def split(self, time):
+    """split"""
+
+  def checkForCollision(self, fred):
+
+    """collision"""
+
+  def loadImages(self, pygame):
+    """images"""
+
+  def move(self, windowHeight):
+
+    """move"""
+
+  def draw(self, surface, pygame):
+    """ draw """
+
+  def __init__(self, slot):
+    """init"""
