aa9d2e8adfd13dabfeb4625899fd34992f09f648
James ODonnell <mr.odonnell.cte@gmail.com>
2021-05-11 12:08:45 -0400

    correct intenting on objects.py template

--- objects.py
+++ objects.py
@@ -1,45 +1,40 @@
 class Fred():
-	"""fred class variables"""
+        """fred class variables"""
+        def reset(self, x):
+                """reset"""
+        def moveLeft(self, leftBound):
+                """moveLeft"""
 
-  def reset(self, x):
-    """reset"""
-
-  def moveLeft(self, leftBound):
-	"""moveLeft"""
-
-  def moveRight(self, rightBound):
-	"""moveRight"""
+        def moveRight(self, rightBound):
+                """moveRight"""
     
 
-  def loadImages(self, pygame):
-	"""loadImages"""
+        def loadImages(self, pygame):
+                """loadImages"""
 
-  def draw(self, surface, time):
-	""" draw """
-	
-  def __init__(self, x):
-    """init"""
+        def draw(self, surface, time):
+                """ draw """
+
+        def __init__(self, x):
+                """init"""
 
 class Barrel():
+        """variables"""
 
-  """variables"""
+        def split(self, time):
+                """split"""
 
-  def split(self, time):
-    """split"""
+        def checkForCollision(self, fred):
+                """collision"""
 
-  def checkForCollision(self, fred):
+        def loadImages(self, pygame):
+                """images"""
 
-    """collision"""
+        def move(self, windowHeight):
+                """move"""
 
-  def loadImages(self, pygame):
-    """images"""
+        def draw(self, surface, pygame):
+                """ draw """
 
-  def move(self, windowHeight):
-
-    """move"""
-
-  def draw(self, surface, pygame):
-    """ draw """
-
-  def __init__(self, slot):
-    """init"""
+        def __init__(self, slot):
+                """init"""
